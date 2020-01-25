c8482805fee0a9d9ef46a4097a8cc053e12cbc7e
Marc Levesque <mlevesque@flodraulic.com>
2020-01-25 10:44:59 -0500

    Initial 2020 project layout

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,42 +1,24 @@
 package frc.robot;
 
-import frc.robot.subsystems.*;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-  private Scheduler scheduler;
-  public static Drive drive;
-
-  public void robotInit() {
-    scheduler = Scheduler.getInstance();
-
-    drive = new Drive();
-  }
+  @Override
+  public void robotInit() {  }
 
   @Override
-  public void robotPeriodic() {
-    scheduler.run();
-  }
+  public void robotPeriodic() { }
 
   @Override
-  public void autonomousInit() {
-    Drive.leftFront.startMotionProfile(MotionPoints.pointStream, 0, ControlMode.MotionMagic);
-    Drive.rightFront.startMotionProfile(MotionPoints.pointStream, 0, ControlMode.MotionMagic);
-  }
+  public void autonomousInit() { }
 
   @Override
-  public void autonomousPeriodic() {
-    
-  }
+  public void autonomousPeriodic() { }
 
   @Override
-  public void teleopPeriodic() {
-  }
+  public void teleopPeriodic() { }
 
   @Override
-  public void testPeriodic() {
-  }
+  public void testPeriodic() { }
 }
