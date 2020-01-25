b1b33c7b906f79ef3a2529284d872b42d0177c5f
Kiersten <45017084+Fuzzydudes@users.noreply.github.com>
2020-01-25 15:13:51 -0500

    PID Aim

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,31 +1,66 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
 
-  private Scheduler scheduler;
-  public static Drive drive;
+  public static Shooter shooter;
+  public static TalonSRX rightFront  = new TalonSRX(48);
+public static TalonSRX leftFront = new TalonSRX(43);
+public static TalonSRX rightBack = new TalonSRX(45);
+public static TalonSRX leftBack = new TalonSRX(46);
+public static Joystick driver = new Joystick(0);
 
   public void robotInit() {
-    scheduler = Scheduler.getInstance();
-
-    drive = new Drive();
+    shooter = new Shooter();
+    shooter.ShooterParent.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
+    shooter.ShooterParent.configContinuousCurrentLimit(42);
+    shooter.ShooterParent.configPeakCurrentLimit(60);
+    shooter.ShooterParent.configPeakCurrentDuration(300);
+    shooter.ShooterChild.configContinuousCurrentLimit(42);
+    shooter.ShooterChild.configPeakCurrentLimit(60);
+    shooter.ShooterChild.configPeakCurrentDuration(300);
   }
 
   @Override
   public void robotPeriodic() {
-    scheduler.run();
+    if (driver.getRawButton(5)) {
+      shooter.target();
+//      double shooterOutput =  shooter.shooter.calculate(((-shooter.ShooterParent.getSelectedSensorPosition()/2048)*600)/1.75,  11000);
+//      shooter.ShooterParent.set(ControlMode.PercentOutput, shooterOutput);
+//      shooter.ShooterChild.set(ControlMode.PercentOutput, shooterOutput);
+      shooter.ShooterParent.set(ControlMode.PercentOutput, .7);
+      shooter.ShooterChild.set(ControlMode.PercentOutput, .7);
+    }else{
+      shooter.ShooterParent.set(ControlMode.PercentOutput, 0);
+      shooter.ShooterChild.set(ControlMode.PercentOutput, 0);
+      double steer = driver.getRawAxis(0);
+      double forward = driver.getRawAxis(2)-driver.getRawAxis(3);
+      leftFront.set(ControlMode.PercentOutput,steer+forward);
+      leftBack.set(ControlMode.PercentOutput,steer+forward);
+      rightBack.set(ControlMode.PercentOutput, steer-forward);
+      rightFront.set(ControlMode.PercentOutput,steer-forward);
+    }
+
+    if(driver.getRawButton(1)){
+      shooter.Indexer.set(ControlMode.PercentOutput, 1);
+    } else {
+      shooter.Indexer.set(ControlMode.PercentOutput, 0);
+    }
+
+    SmartDashboard.putNumber("CurrentDraw", shooter.ShooterChild.getStatorCurrent());
+    SmartDashboard.putNumber("RPM", ((-shooter.ShooterParent.getSelectedSensorVelocity()/2048)*600)*1.75);
   }
 
   @Override
   public void autonomousInit() {
-    Drive.leftFront.startMotionProfile(MotionPoints.pointStream, 0, ControlMode.MotionMagic);
-    Drive.rightFront.startMotionProfile(MotionPoints.pointStream, 0, ControlMode.MotionMagic);
-  }
+      }
 
   @Override
   public void autonomousPeriodic() {
---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.Robot;
+
+public class Shooter extends Subsystem {
+    public TalonSRX ShooterParent  = new TalonSRX(47);
+    public TalonSRX ShooterChild = new TalonSRX(60);
+    public TalonSRX Indexer = new TalonSRX(62);
+    public PIDController controller = new PIDController(0.1,0,0.01); //p = .1, i = 0, d = 0
+    public PIDController shooter = new PIDController(0.1, 0, 0);
+
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    public double getX(){
+        NetworkTableEntry tx = table.getEntry("tx");
+        return tx.getDouble(0.0);
+    }
+
+    public boolean canSeeTarget (){
+        NetworkTableEntry tv = table.getEntry("tv");
+        return tv.getDouble(0) > 0;
+    }
+
+    public void getPID(){
+
+    }
+
+    public void target (){
+        System.out.println("Shooter can see target " + canSeeTarget());
+        if (canSeeTarget()){
+            double x = getX();
+            double output = -controller.calculate(x, 0);
+            Robot.rightFront.set(ControlMode.PercentOutput, output);
+            Robot.rightBack.set(ControlMode.PercentOutput,output);
+            Robot.leftBack.set(ControlMode.PercentOutput,output);
+            Robot.leftFront.set(ControlMode.PercentOutput,output);
+
+            //double shooterOutput =  shooter.calculate(((-ShooterParent.getSelectedSensorPosition()/2048)*600)/1.75,  11000);
+            //ShooterParent.set(ControlMode.PercentOutput, .9);
+            //ShooterChild.set(ControlMode.PercentOutput, .9);
+
+        }else{
+            //ShooterParent.set(ControlMode.PercentOutput, 0);
+            //ShooterChild.set(ControlMode.PercentOutput, 0);
+            Robot.leftFront.set(ControlMode.PercentOutput,0);
+            Robot.leftBack.set(ControlMode.PercentOutput,0);
+            Robot.rightBack.set(ControlMode.PercentOutput,0);
+            Robot.rightFront.set(ControlMode.PercentOutput,0);
+        }
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+}
